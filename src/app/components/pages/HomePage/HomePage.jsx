d1bfd5c1446dc5db46a42fd643d1517000be5eb3
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-01-20 22:30:06 +0600

    add base layout

---
+++ src/app/components/pages/HomePage/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+    return <section className="main-page">Контент страницы</section>;
+};
+
+export default HomePage;
---
+++ src/app/layouts/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HomePage from "../components/pages/HomePage";
+
+const main = () => {
+    return (
+        <>
+            {/* <NavBar/> */}
+            <main className="page">
+                <HomePage />
+            </main>
+            {/* <Footer/> */}
+        </>
+    );
+};
+
+export default main;
