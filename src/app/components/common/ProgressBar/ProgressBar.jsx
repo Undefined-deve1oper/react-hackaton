92ab7e7698ddd17629c16aec66c9fa620b16d1d4
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-01-21 19:57:24 +0600

    add circle progress bar layout

--- src/app/components/common/ProgressBar/ProgressBar.jsx
+++ src/app/components/common/ProgressBar/ProgressBar.jsx
@@ -1,58 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ProgressBar = ({ percentages, color, title }) => {
+const ProgressBar = ({ percentages, color, title, type = "circle" }) => {
     return (
-        <div className="progress-bar">
-            <div
-                style={{ width: percentages + "%" }}
-                className="progress-bar__item"
-            >
-                <div className="progress-bar__label">
-                    {title} - {`${percentages}%`}
-                </div>
-            </div>
-            {/* {type === "circle" ? (
-                <div className="progress-bar_circle">
-                    <svg
-                        strokeDashoffset={percentages}
-                        className="progress-bar__svg"
-                    >
-                        <circle className="svg__track" />
-                        <circle className="svg__indicator" />
-                    </svg>
-                    <div className="progress-bar__label">
-                        <span className="progress-bar__label-text">
-                            {percentages}%
-                        </span>
-                    </div>
-                    <div className="progress-bar__title">
-                        <h2 className="progress-bar__title-text">{title}</h2>
+        <>
+            {type === "circle" ? (
+                <div class="circle-wrap">
+                    <div class="circle">
+                        <div class="mask full">
+                            <div class="fill"></div>
+                        </div>
+                        <div class="mask half">
+                            <div class="fill"></div>
+                        </div>
+                        <div class="inside-circle"> {`${percentages}%`} </div>
                     </div>
                 </div>
             ) : (
-                <div className="progress-bar__linear">
-                    <progress
-                        className="html5"
-                        max="100"
-                        value={percentages}
-                    ></progress>
-                    <span className="progress-bar__linear-text">
-                        {percentages}%
-                    </span>
-                    <div className="progress-bar__title">
-                        <h2 className="progress-bar__title-text">{title}</h2>
+                <div className="progress-bar">
+                    <div
+                        style={{ width: percentages + "%" }}
+                        className="progress-bar__item"
+                    >
+                        <div className="progress-bar__label">
+                            {title} - {`${percentages}%`}
+                        </div>
                     </div>
                 </div>
-            )} */}
-        </div>
+            )}
+        </>
     );
 };
 
 ProgressBar.propTypes = {
     percentages: PropTypes.number.isRequired,
     title: PropTypes.string,
-    type: PropTypes.string
+    type: PropTypes.string,
+    color: PropTypes.string
 };
 
 export default ProgressBar;
