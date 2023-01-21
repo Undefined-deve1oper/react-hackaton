916f7df66f6d53157e37b81c02dc11db942a8a1f
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-01-21 23:48:21 +0600

    add layout ui login forms

---
+++ src/app/components/ui/Forms/Login/SignInForm/SignInForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignInForm = () => {
+    return <form></form>;
+};
+
+export default SignInForm;
---
+++ src/app/components/ui/Forms/Login/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUpForm = () => {
+    return <form></form>;
+};
+
+export default SignUpForm;
